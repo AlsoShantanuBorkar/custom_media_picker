622087078d66cf3bbf411b4d19f7927dc239fe29
Shantanu Borkar <shantanu.b@arishti.in>
2024-08-10 16:46:37 +0530

    add: AlbumListScreen Created

---
+++ lib/album_list_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import 'package:photo_manager/photo_manager.dart';
+
+class AlbumListScreen extends StatelessWidget {
+  const AlbumListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Album List"),
+      ),
+      body: FutureBuilder(
+        future: _getAlbumList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<AssetPathEntity> albumList = snapshot.data!;
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: albumList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      title: Text(albumList[index].name),
+                    ),
+                  );
+                });
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Future<List<AssetPathEntity>> _getAlbumList() async {
+    PermissionState permissionState =
+        await PhotoManager.requestPermissionExtend();
+    if (permissionState.hasAccess || permissionState.isAuth) {
+      List<AssetPathEntity> albumList =
+          await PhotoManager.getAssetPathList(type: RequestType.common);
+      return albumList;
+    } else {
+      await PhotoManager.openSetting();
+      return await _getAlbumList();
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:custom_media_picker/album_list_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,11 +10,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const MaterialApp(home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Media Picker"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const AlbumListScreen(),
+                ),
+              );
+            },
+            child: const Text("Pick Image")),
       ),
     );
   }
