861e981bbf2bbfb09557927fe22c1d22a85d986a
Shantanu Borkar <shantanu.b@arishti.in>
2024-08-12 16:17:57 +0530

    refactor: imageList->mediaList

--- lib/screens/media_list_screen.dart
+++ lib/screens/media_list_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:custom_media_picker/screens/media_preview_screen.dart';
 import 'package:flutter/material.dart';
@@ -17,91 +18,126 @@ class MediaListScreen extends StatefulWidget {
 class _MediaListScreenState extends State<MediaListScreen> {
   @override
   void initState() {
-    _getImageList();
+    _getMediaList();
     super.initState();
   }
 
   int currentPage = 0;
 
-  List<AssetEntity> imageList = [];
+  List<AssetEntity> mediaList = [];
 
   bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     final double imageSize = MediaQuery.of(context).size.width / 2;
-
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.album.name),
-        ),
-        body: SingleChildScrollView(
-          child: Column(children: [
+      appBar: AppBar(
+        title: Text(widget.album.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
             GridView.builder(
               physics: const NeverScrollableScrollPhysics(),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
               ),
               shrinkWrap: true,
-              itemCount: imageList.length,
+              itemCount: mediaList.length,
               itemBuilder: (BuildContext context, int index) {
-                return FutureBuilder(
-                    future: imageList[index].thumbnailDataWithSize(
-                        ThumbnailSize(imageSize.toInt(), imageSize.toInt())),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => MediaPreviewScreen(
-                                  media: imageList[index],
-                                ),
-                              ),
-                            );
-                          },
-                          child: Image.memory(
-                            snapshot.data!,
-                            height: imageSize,
-                            width: imageSize,
-                            fit: BoxFit.cover,
-                          ),
-                        );
-                      }
-                      return const Center(child: CircularProgressIndicator());
-                    });
+                return SizedBox(
+                    height: imageSize,
+                    width: imageSize,
+                    child: MediaThumbnailPreview(media: mediaList[index]));
               },
             ),
             const SizedBox(height: 20),
             if (isLoading) const CircularProgressIndicator(),
-            if (!isLoading || imageList.isNotEmpty)
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(shape: const CircleBorder()),
-                  onPressed: () {
-                    setState(() {
-                      currentPage++;
-                      _getImageList(page: currentPage);
-                    });
-                  },
-                  child: const Icon(Icons.add))
-          ]),
-        ));
+            if (!isLoading || mediaList.isNotEmpty)
+              IconButton(
+                style: ElevatedButton.styleFrom(shape: const CircleBorder()),
+                onPressed: () {
+                  setState(() {
+                    currentPage++;
+                    _getMediaList(page: currentPage);
+                  });
+                },
+                icon: const Icon(Icons.add),
+              )
+          ],
+        ),
+      ),
+    );
   }
 
-  Future<void> _getImageList({int page = 0}) async {
+  Future<void> _getMediaList({int page = 0}) async {
     isLoading = true;
     if (mounted) {
       setState(() {});
     }
-    List<AssetEntity> images =
+    List<AssetEntity> media =
         await widget.album.getAssetListPaged(page: page, size: 50);
 
-    imageList.addAll(images);
+    mediaList.addAll(media);
     isLoading = false;
 
     if (mounted) {
       setState(() {});
     }
+  }
+}
+
+class MediaThumbnailPreview extends StatelessWidget {
+  const MediaThumbnailPreview({super.key, required this.media});
+  final AssetEntity media;
+  @override
+  Widget build(BuildContext context) {
+    final double imageSize = MediaQuery.of(context).size.width / 2;
+    return FutureBuilder(
+        future: media.thumbnailDataWithSize(
+            ThumbnailSize(imageSize.toInt(), imageSize.toInt())),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MediaPreviewScreen(
+                      media: media,
+                    ),
+                  ),
+                );
+              },
+              child: SizedBox(
+                height: imageSize,
+                width: imageSize,
+                child: Stack(
+                  children: [
+                    Image.memory(
+                      snapshot.data!,
+                      height: imageSize,
+                      width: imageSize,
+                      fit: BoxFit.cover,
+                    ),
+                    if (media.type == AssetType.video)
+                      Container(
+                        height: imageSize,
+                        width: imageSize,
+                        color: Colors.black54.withOpacity(.5),
+                        child: const Icon(
+                          Icons.play_arrow,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 }
