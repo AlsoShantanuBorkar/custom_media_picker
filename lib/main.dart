d58863b5cf6ac598cec670f346122e9264bb5dcc
Shantanu Borkar <shantanu.b@arishti.in>
2024-08-12 12:53:35 +0530

    remove: debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,10 @@ void main() {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
 
